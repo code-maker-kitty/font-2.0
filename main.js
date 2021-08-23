fd51e93000c4564e2d1d746b469aa354faae9881
code-maker-kitty <77761014+code-maker-kitty@users.noreply.github.com>
2021-08-23 16:49:37 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
 
-    canvas = createCanvas(550, 550);
+    canvas = createCanvas(850, 550);
     canvas.position(560,150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -12,11 +17,23 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX +"rightWristX ="+ rightWristX + "difference =" + difference);
     }
 }
 function modelLoaded(){
     console.log('poseNet is Initialized!');
 }
 function draw() {
-    background('#00b4f7');
+    background('#dae511');
+    document.getElementById("font_sides").innerHTML = "Font Size of the Text will be = " + difference +"px";
+    textSize(difference);
+    fill("#31e248");
+    text("hi i am leo",noseX,noseY);
 }
